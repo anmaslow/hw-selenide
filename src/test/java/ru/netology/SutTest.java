129a3c2d4e2b777b49d01580a0dc13101e3cf025
Anastasia <ann.maslow@gmail.com>
2020-02-11 22:14:35 +0300

    add test

---
+++ src/test/java/ru/netology/SutTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SutTest {
+
+    @Test
+    @DisplayName("Should submit request")
+    void shouldSubmitRequest() {
+
+        open("http://localhost:9999");
+
+        $$("#root .form-field").find(Condition.hidden);
+        $("[data-test-id=city] input").setValue("Москва");
+
+        LocalDate date = LocalDate.now().plusDays(3);
+        $("[data-test-id=date] input").setValue(String.valueOf(date));
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79051112233");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000);
+    }
+}
